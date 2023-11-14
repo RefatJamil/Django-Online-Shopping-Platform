aa103fffa3db6a62ee95ca6ae81c6bd5523422c0
Refat Jamil <rifatjamil54@gmail.com>
2023-11-14 21:45:34 +0600

    update project name

--- OShop/OShop/wsgi.py
+++ OShop/OShop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ShopiMart project.
+WSGI config for OShop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShopiMart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OShop.settings')
 
 application = get_wsgi_application()
